579b3f4ce0203dd3382b1426ade9899adc34a950
Matteo Sartini <MATTEO.SARTINI@local.ittcesena.it>
2025-11-07 09:23:26 +0100

    Merge Cd

--- Es_classi/Cd.cs
+++ Es_classi/Cd.cs
@@ -8,5 +8,45 @@ namespace Es_classi
 {
     class Cd
     {
+        // Attributi
+        private string titolo;
+        private string autore;
+        private List<string> listaBrani;
+        private double durataTotale; // in minuti
+        // Costruttore
+        public Cd(string titolo, string autore, List<string> listaBrani, double durataTotale)
+        {
+            this.titolo = titolo;
+            this.autore = autore ;
+            this.listaBrani = listaBrani;
+            this.durataTotale = durataTotale;
+        }
+        // Metodi Getter
+        public string Titolo
+        {
+            set{ titolo = value; }
+            get{ return titolo; }
+        }
+        public string Autore
+        {
+            set{ autore = value; }
+            get{ return autore; }
+        }
+        public List<string> GetListaBrani()
+        {
+            return listaBrani;
+        }
+        public double GetDurataTotale()
+        {
+            return durataTotale;
+        }
+        // Metodo per visualizzare le informazioni del CD
+        public void VisualizzaInfo()
+        {
+            Console.WriteLine("Titolo: " + titolo);
+            Console.WriteLine("Artista: " + autore);
+            Console.WriteLine("Lista Brani: " + listaBrani);
+            Console.WriteLine("Durata Totale: " + durataTotale + " minuti");
+        }
     }
 }
