6e40de760785008c166396f8c9de060a30341c28
Eros Soldati <EROS.SOLDATI@local.ittcesena.it>
2025-11-07 08:29:13 +0100

    Versione Finale BranoEros

--- Es_classi/Brano.cs
+++ Es_classi/Brano.cs
@@ -8,5 +8,50 @@ namespace Es_classi
 {
     class Brano
     {
+        private string titolo;
+        private string autore;
+        private double durata;
+
+        public Brano(string _titolo, string _autore, double _durata)
+        {
+            _titolo = titolo;
+            _autore = autore;
+            _durata = durata;
+        }
+
+        public string Titolo
+        {
+            get { return titolo; }
+            set { titolo = value; }
+        }
+
+        public string Autore
+        {
+            get { return autore; }
+            set { autore = value; }
+        }
+
+        public double Durata
+        {
+            get { return durata; }
+            set { durata = value; }
+        }
+
+        public string toString()
+        {
+            return "Titolo: " + titolo + ", Autore: " + autore + ", Durata: " + durata + " minuti.";
+        }
+
+        public bool shortSong(double durata2)
+        {
+            if (durata < durata2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
